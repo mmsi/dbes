3d748674f12655eee9e2a50719797cdc4e91be29
Joel Morgan <joel@jacklaptop.(none)>
2010-01-21 22:27:50 -0600

    further config cleanups  Committer: Joel Morgan <joel@jacklaptop.(none)>
     On branch master
     Changes to be committed:
    	modified:   main.c

--- main.c
+++ main.c
@@ -21,7 +21,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
  */
- 
+
 #include<stdio.h>
 #include<fcntl.h>
 #include<sys/mman.h>
@@ -31,6 +31,12 @@
 #define JUMPER_PAGE  0x22800000
 #define POS			 0
 #define PRES		 1
+
+/*configuration file name*/
+#define CONFIG_NAM "config"
+
+/*length of configuration file (characters)*/
+#define MAX_CONFIG 27
 
 /*prototypes*/
 int MainLoop();
@@ -48,16 +54,16 @@ int main()
 {
 	int i, c;
 	char *jump6;
-	
-	
+
+
 	devmem = open("dev/mem", O_RDWR|O_SYNC);
-	
+
 	/*init all segments*/
 	start = mmap(0, 4096, PROT_READ|PROT_WRITE, MAP_SHARED,
 				 devmem, DIO_PAGE);
 	jump6 = mmap(0, 4096, PROT_READ|PROT_WRITE, MAP_SHARED,
 				devmem, JUMPER_PAGE);
-	
+
 	h_status = (*jump6 & 0x1);
 	printf("initializing sensors...\n");
 	Sensors(0);
@@ -66,48 +72,45 @@ int main()
 	Hyd_Control(local_control);
 	printf("initializing interface/arbitration...\n");
 	Arbitor(status, &local_control);
-	
+
 	/*configuration prompt*/
 	if (h_status == 1 ) {
 		/*master*/
+		fp = fopen(CONFIG_NAM, "r");
+		if (fp >= 0) {
+			for (i=0; i<=MAX_CONFIG; i++)
+				calib[i] = getc(fp);
+			fclose(fp);
+		} else {
+			Configuration();
+			goto bypass; /*FIXME eliminate goto
+			need to jump past the prompt below*/
+		}
 		printf("would you like to enter configuration? y/n   ");
 		while ((c = getchar()) != 'y' || 'n')
-		if (c == 'y') {
-			//FIXME grab old config here so user can compare
+		if (c == 'y')
 			Configuration();
-		} else {
-			fp = fopen("config", "r");
-			if  (fp < 0) {
-				fclose(fp);
-				Configuration();
-			} else {
-				for (i = 0; i <= 27; i++) {
-					calib[i] = getc(fp);
-				}
-				fclose(fp);
-			}
-		}
 	} else {
 		/*slave*/
-		fp = fopen("config", "r");
+		fp = fopen(CONFIG_NAM, "r");
 		if (fp < 0) {
 			printf("no configuration file");
 			exit(0); //FIXME better alerting of operator, maybe over CAN
 		}
-		for (i = 0; i <= 27; i++) {
+		for (i = 0; i <= MAX_CONFIG; i++)
 			calib[i] = getc(fp);
 		fclose(fp);
-		}
 	}
 
+	bypass:
 	/*clear init*/
 	local_control.function = 0x0000;
-	
+
 	/*infinite loop unless fatal error*/
 	do {
 		i = MainLoop();
 	} while (i == 0);
-	
+
 	/*clean restart*/
 	//system("init 6"); FIXME temporarily disabled for debugging
 }
@@ -119,14 +122,14 @@ int MainLoop()
 		Contingency();
 		return 0;
 	}
-		
+
 	/*get control orders*/
 	if (Arbitor(status, &local_control) < 0) {
 		printf("int/arb error...\n");
 		Contingency();
 		return 0;
 	}
-		
+
 	/*actuate hydraulics*/
 	Hyd_Control(local_control);
 
@@ -136,13 +139,13 @@ int MainLoop()
 int Contingency(void)
 {
 	int ret;
-	
+
 	local_control.function = 0x0001;
 	printf("entering contingency mode...\nplease correct problem");
 	printf(" and reset system\n"); //FIXME ui() should handle this and the reset
 	Hyd_Control(local_control); // shut off all valves
 	Arbitor(status, &local_control); //send contingency over CAN
-	
+
 	/*reset handler*/
 	printf("press 'r' to reset: ");
 	do {
@@ -150,7 +153,7 @@ int Contingency(void)
 	} while (ret != 'r');
 	local_control.function = 0x0000;
 	printf("\n\nresuming normal mode...\n");
-	 
+
 	return;
 }
 
@@ -159,35 +162,36 @@ int Configuration(void)
 	char str[20];
 	int raw;
 	char c;
-	
+
 	fp = fopen("config", "w"); //FIXME add error handler
-	
+	if (fp < 0) {
+	}
+
 	/*address*/
-	printf("current address is: %c%c%c\n", calib[0],
-			calib[1], calib[2]);
+	printf("current address is: %c3\n", calib[0]);
 	printf("would you like to change the address? y/n   ");
 	while ((c = getchar()) != 'y' || 'n') {
 		printf("\njust enter 'y' or 'n', moron");
 	}
 	if (c == 'y') {
 		printf("\nplease enter a three digit number:   ");
-		while ((strlen(gets(&str))) != 3) {//FIXME also check for numericness
+		while ((strlen(gets(&str))) != 3) {//FIXME check for numericness
 			printf("\ntry again - 3 digits, press enter!");
 		}
-	}
-	//FIXME - store address
+		fprintf(fp, "%s3", str);
+	} else
+		fprintf(fp, "%s3", calib[0]);
 	
 	/*pressure*/
 	printf("\nset system pressure to zero and wait for the prompt\n");
 	sleep(5);
 	Sensor_cal(raw, PRES);
 	printf("zero set at %u\n", raw);
-	//FIXME write to file
+	fprintf(fp, "%      ", raw);
 	printf("disconnect return hose\n");
 	printf("set system to full pressure and enter the gauge ");
 	printf("reading\n");
-	(float)(Sensor_cal(raw, PRES) / (atoi(gets(&str))));
-
+	fprintf(fp, "%    ", (float)(Sensor_cal(raw, PRES) / (atoi(gets(&str)))));
 
 	/*Position Transducer*/
 	/*offset*/
@@ -203,7 +207,7 @@ int Configuration(void)
 	printf("zero at %u\n", raw);
 	//FIXME lseek() to proper place
 	fprintf(fp, "%4u", raw);
-	
+
 	/*scale*/
 	printf("press enter when height is 24 to 36 inches\n");
 	local_control.function = 0x2;
@@ -216,8 +220,8 @@ int Configuration(void)
 	printf("enter height as xx.xx:   ");
 	gets(&str); //FIXME add string test
 	raw = (float)(((Sensor_cal(POS)) - raw) / (atof(gets(&str))));
-	fprintf("data", "%4.3f", raw);
-	
+	fprintf(fp, "%4.3f", raw);
+
 	local_control.function = 0x4;
 	local_control.rate = 0xFF;
 	Hyd_Control(local_control);
